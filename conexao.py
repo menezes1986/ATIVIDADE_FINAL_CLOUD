9f40e6107a182a1e1724134164ddbe32c3d59f8a
menezes1986 <122611318+menezes1986@users.noreply.github.com>
2024-09-18 17:29:43 +0000

    commit db

---
+++ conexao.py
@@ -0,0 +1,10 @@
+import mysql.connector 
+
+db_config2 = {
+    'host':'localhost3306',
+    'user':'root',
+    'password':'13032018',
+    'database':'cloud_computing',
+}
+
+#IMPLEMENTATION INACIO MENEZES
